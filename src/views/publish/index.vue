149c0ebb8b83122dc8d0cf15fa47848eea3616d4
Vnho <VnhoMeng@163.com>
2020-03-02 15:14:36 +0800

    文章发布-模板渲染完成

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,23 +1,63 @@
 <template>
   <div class="publish">
+    <!-- 发布文章区域 -->
     <el-card class="box-card">
+      <!-- 表头 -->
       <div slot="header" class="clearfix">
         <span>发布文章</span>
       </div>
+      <!-- 表单内容 -->
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="文章标题">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="文章内容">
+          <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="form.region" placeholder="请选择频道">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="文章封面">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="单图"></el-radio>
+            <el-radio label="三图"></el-radio>
+            <el-radio label="无图"></el-radio>
+            <el-radio label="自动"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">提交</el-button>
+          <el-button>草稿</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Publish',
   data () {
-    return {}
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
   },
-
-  created () {},
-
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
